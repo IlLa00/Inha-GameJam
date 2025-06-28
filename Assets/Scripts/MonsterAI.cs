f1dba999e9e1614efe54fd57fae974d365f5e22b
KimSuhyeock <ladon22@naver.com>
2025-06-28 17:10:31 +0900

    0628 17:09 배경, 몬스터 리스폰,
    배경 구현
    몬스터 리스폰 틀만 만듦

--- Assets/Scripts/MonsterAI.cs
+++ Assets/Scripts/MonsterAI.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,7 +10,6 @@ public class Monster : BaseCharater
     private Animator animator;
     void Start()
     {
-        
     }
 
     // Update is called once per frame
