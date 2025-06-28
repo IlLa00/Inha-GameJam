2d53af4e9e26517c28693a8d725e182e3678bed0
Hyun Jun <154495800+IlLa00@users.noreply.github.com>
2025-06-28 13:54:51 +0900

    0628_Interactive Object
    13:53
    발전기 일단 생성
    캐비닛은 캐릭터 처리 이후에
    아이템은 회의 필요
    탈출구는 발전기 끝나고

---
+++ Assets/Scripts/Exit.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Exit : InteractiveObject
+{
+    public bool CanEscape = false;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    protected override void OnInteractive()
+    {
+        Debug.Log("Starting Exit OnInteractive");
+        
+        // 발전기가 다 켜져있는지. 안켜져있으면 얼리 리턴
+
+    }
+}
---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Obstacle : InteractiveObject
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    protected override void OnInteractive()
+    {
+        // 부모에서 플레이어 널값 검사
+        Debug.Log("Starting Obstacle OnInteractive");
+
+        //var PlayerController = player.GetComponent<MonoBehaviour>(); // 플레이어 컨트롤러 비활성화
+        //if (PlayerController != null)
+        //    PlayerController.enabled = false;
+
+        // 시야 변환
+
+    }
+}
