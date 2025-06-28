ffbf98d18e7f7da80ca7d8268b3febcd630a9dd1
dogyeng <dogyeo114@gmail.com>
2025-06-28 17:39:56 +0900

    06_28 5:40 character
    몬스터 AI 일분구현

--- Assets/Scripts/MonsterAI.cs
+++ Assets/Scripts/MonsterAI.cs
@@ -1,20 +1,100 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Monster : BaseCharater
 {
     // Start is called before the first frame update
 
-    private Animator animator;
+    public Transform GroundCheck;
+    private Rigidbody2D Rigid2D;
+    [SerializeField] LayerMask GroundLayer;
+
+    public float GroundCheckDistance = 1f;
+    private bool isMovingRight = true;
+
+
+    [SerializeField] private LayerMask PlayerLayer;
+    [SerializeField] private float FindRange = 5f;
+    [SerializeField] private float RunSpeed = 2f;
+
+    private Transform player;
+    private bool isPlayerDetected = false;
+    private bool isChasing = false;
+    private bool isWaiting = false;
+
     void Start()
     {
-        
+        Rigid2D = GetComponent<Rigidbody2D>();
+
+        this.HP = 1;
+        this.Atk = 2;
+        this.Speed = 1f; // 기본 스피드
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Sencing();
+        HandlChase();
+        Patrol();
+    }
+    void Patrol() //raycast로 땅 position 
+    {
+        RaycastHit2D groundInfo = Physics2D.Raycast(GroundCheck.position, Vector2.down, GroundCheckDistance, GroundLayer);
+        if(!groundInfo.collider)
+        {
+            flip();
+        }
+
+        Rigid2D.velocity = new Vector2((isMovingRight ? 1 : -1) * Speed, Rigid2D.velocity.y);
+    }
+    void flip()
+    {
+        isMovingRight = !isMovingRight;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+    void Sencing()
+    {
+        if (isPlayerDetected)
+            return;
+
+        Collider2D hit = Physics2D.OverlapCircle(transform.position, FindRange, PlayerLayer);
+        if(hit != null && hit.CompareTag("Player"))
+        {
+            Debug.Log("33");
+            player = hit.transform;
+            isPlayerDetected = true;
+
+            if(!isWaiting)
+            {
+                StartCoroutine(WaitThenChase());
+            }
+        }
+        else { isPlayerDetected = false; }
+    }
+    void HandlChase()
+    {
+        if (!isChasing || player == null)
+            return;
+
+        transform.position = Vector2.MoveTowards(transform.position, player.position, RunSpeed * Time.deltaTime);
+    }
+    IEnumerator WaitThenChase()
+    {
+        isWaiting = true;
+
+        yield return new WaitForSecondsRealtime(3f); // 3초 대기
+
+        isChasing = true;
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, FindRange);
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,7 +9,7 @@ public class PlayerController : BaseCharater
     [SerializeField]
     private float WalkForce = 10.0f;
     [SerializeField]
-    private float JumpForce = 10.0f;
+    private float JumpForce = 5.0f;
     [SerializeField]
     private float MaxSpeed = 2.0f;
     [SerializeField]
@@ -22,7 +22,6 @@ public class PlayerController : BaseCharater
     private float RunMaxSpeed = 4.0f;
 
     Rigidbody2D Rigid2D;
-    Animator Animator;
 
     public event Action<int> UpdateHP;
 
@@ -30,12 +29,21 @@ public class PlayerController : BaseCharater
     void Start()
     {
         this.Rigid2D = GetComponent<Rigidbody2D>();
-        this.Animator = GetComponent<Animator>();
+        base.animator = GetComponent<Animator>();
 
         this.HP = 10;
         this.Atk = 30;
         CurrentNoiseLevel = NoiseLevel;
         UpdateHP?.Invoke(this.HP);
+    }
+    
+    public float GetNoiseLevel()
+    {
+        return NoiseLevel;
+    }
+    public float GetCurrentNoiseLevel()
+    {
+        return CurrentNoiseLevel;
     }
 
     public override void ChangeState(State newState)
@@ -49,85 +57,93 @@ public class PlayerController : BaseCharater
         switch (newState)
         {
             case State.Idle:
-                Animator.SetFloat("Speed", 0);
+                base.animator.SetFloat("Speed", 0);
                 break;
             case State.Walk:
-                Animator.SetFloat("Speed", 0.5f);
+                base.animator.SetFloat("Speed", 0.1f);
                 break;
             case State.Run:
-                Animator.SetFloat("Speed", 1f);
+                base.animator.SetFloat("Speed", 0.6f);
                 break;
             case State.Jump:
-                Animator.SetBool("Jump", true);
+                base.animator.SetBool("IsJumping", true);
                 break;
             case State.Die:
-                Animator.SetBool("IsDead", true);
+                base.animator.SetTrigger("Die");
+                break;
+            case State.Attack:
+                base.animator.SetTrigger("Attack");
+                break;
+            case State.Be_Attacked:
+                base.animator.SetTrigger("be_Attacked");
                 break;
         }
     }
+
     // Update is called once per frame
     void Update()
     {
         HandleInput();
 
         int key = 0;
-        if(Input.GetKey(KeyCode.RightArrow)) { key = 1; }
-        if(Input.GetKey(KeyCode.LeftArrow)) { key = -1; }
+        if (Input.GetKey(KeyCode.RightArrow)) { key = 1; }
+        if (Input.GetKey(KeyCode.LeftArrow)) { key = -1; }
 
-        bool isRunning = Input.GetKey(KeyCode.LeftShift);
+        bool isRunning = Input.GetKey(KeyCode.LeftShift); //달리는지 확인 후 현재 force를 계산함
         float currentForce = isRunning ? RunForce : WalkForce;
         float currentMaxSpeed = isRunning ? RunMaxSpeed : MaxSpeed;
 
         this.Rigid2D.AddForce(transform.right * key * currentForce); // 좌우 움직임
         float velX = Mathf.Abs(this.Rigid2D.velocity.x);
 
-        if(velX > currentMaxSpeed) // 속도제한
+        if (velX > currentMaxSpeed) // 속도제한
         {
             this.Rigid2D.velocity = new Vector2(Mathf.Sign(this.Rigid2D.velocity.x) * currentMaxSpeed, this.Rigid2D.velocity.y);
         }
 
-        if(key != 0) // 방향전환
+        if (key != 0) // 방향전환
         {
             float currnetX = Mathf.Abs(transform.localScale.x);
             transform.localScale = new Vector3(currnetX * key, transform.localScale.y, transform.localScale.z);
         }
 
+        // speed에 따라 애니메이션이 바뀜
         float normalizedSpeed = velX / RunMaxSpeed;
-        Animator.SetFloat("Speed", normalizedSpeed);
+        base.animator.SetFloat("Speed", normalizedSpeed);
     }
-
-    private void FixedUpdate()
+   
+    void FixedUpdate()
     {
-        // 바닥에 닿아있는 상태인지 체크
-        if (Rigid2D.velocity.y <= 0) //내려갈때만 스캔
-        {
-            Debug.DrawRay(Rigid2D.position, Vector3.down, new Color(0, 1, 0));
-            RaycastHit2D rayHit = Physics2D.Raycast(Rigid2D.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
-            if (rayHit.collider != null)
-            {
-                if (rayHit.distance < 0.6f)
-                {
-                    Animator.SetBool("IsJumping", false);
-                }
-            }
-        }
-
+        bool grounded = IsGrounded();
+        animator.SetBool("IsJumping", !grounded);  // 땅에 있으면 false, 공중이면 true
     }
-
     void HandleInput()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow) && !Animator.GetBool("IsJumping")) //점프
+        if (Input.GetKeyDown(KeyCode.UpArrow) && IsGrounded()) //점프
         {
             this.Rigid2D.AddForce(Vector2.up * this.JumpForce, ForceMode2D.Impulse);
-            Animator.SetBool("IsJumping", true);
+            ChangeState(State.Jump);
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             //플랫포머 내려오기?
+            ChangeState(State.Jump);
         }
         else if (Input.GetKeyDown(KeyCode.F))
         {
             // 상호작용 키
+
         }
+    }
+    bool IsGrounded()
+    {
+        float rayOffsetY = -0.5f; // 필요에 따라 조정
+        Vector2 origin = Rigid2D.position + new Vector2(0, rayOffsetY);
+        float rayLength = 0.2f;
+
+        RaycastHit2D hit = Physics2D.Raycast(origin, Vector2.down, rayLength, LayerMask.GetMask("Platform"));
+        Debug.DrawRay(origin, Vector2.down * rayLength, Color.green);
+
+        return hit.collider != null;
     }
 }
