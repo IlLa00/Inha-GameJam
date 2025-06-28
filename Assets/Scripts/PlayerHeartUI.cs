f8d284a3c62589770b8b29ceab7ff99d1f54b5a2
KimSuhyeock <ladon22@naver.com>
2025-06-28 13:57:49 +0900

    0628 13:57 체력 ui
    체력 ui 구현

--- Assets/Scripts/PlayerHeartUI.cs
+++ Assets/Scripts/PlayerHeartUI.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHeartUI : MonoBehaviour
 {
@@ -10,20 +11,59 @@ public class PlayerHeartUI : MonoBehaviour
     private Sprite half_Heart;
     [SerializeField]
     private Sprite full_Heart;
+
     [SerializeField]
-    private GameObject HeartPrefabs;
+    private GameObject HeartPrefab;
     [SerializeField]
-    private Transform Container;
+    private Transform HeartContainer;
+
+    [SerializeField]
+    private PlayerController player;
+    
+    private List<Image> HeartImages = new List<Image>();
+
+    //private void Awake()
+    //{
+    //    player.UpdateHP += UpdateHP_UI;
+    //}
+
+    private void Awake()
+    {
+        player.UpdateHP += UpdateHP_UI;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log(player.HP);
+        int HeartCount = Mathf.CeilToInt(player.HP * 0.5f);
+        Debug.Log(HeartCount);
+        for (int i = 0; i < HeartCount; i++)
+        {
+            GameObject heart = Instantiate(HeartPrefab, HeartContainer.transform);
+            HeartImages.Add(heart.GetComponent<Image>());
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UpdateHP_UI(int playerHP)
     {
-        
+        int currentHP = playerHP;
+        for (int i = 0; i < HeartImages.Count; i++)
+        {
+            if(currentHP >= 2)
+            {
+                HeartImages[i].sprite = full_Heart;
+                currentHP -= 2;
+            }
+            else if (currentHP == 1)
+            {
+                HeartImages[i].sprite = half_Heart;
+                currentHP -= 1;
+            }
+            else
+            {
+                HeartImages[i].sprite = empty_Heart;
+            }
+        }
     }
 }
